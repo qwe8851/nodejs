c85763b936f091b837e30e09d3616670b1dfd8e8
qwe8851 <dgh07027@naver.com>
2023-01-30 20:21:52 +0900

    서버에서 html파일 전송 & nodemon 설치

--- basic/server.js
+++ basic/server.js
@@ -5,6 +5,14 @@ app.listen(8080, function(){
     console.log('listening on 8080');
 });
 
-app.get('/pet', function(req, res){
-    req.send('펫용품을 쇼핑할 수 있는 페이지 입니다.');
+app.get('/pet', function(요청, 응답){
+    응답.send('펫용품을 쇼핑할 수 있는 페이지 입니다.');
+});
+
+app.get('/beauty', function (요청, 응답) {
+    응답.send('뷰티용품을 쇼핑할 수 있는 페이지 입니다.');
+});
+
+app.get('/', function (요청, 응답) {
+    응답.sendFile(__dirname + '/index.html');
 });
