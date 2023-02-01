af13aeccdb867f77755f4408991aee53964d3162
qwe8851 <dgh07027@naver.com>
2023-02-01 21:06:15 +0900

    상세페이지 구현 (URL parameter)

--- basic/server.js
+++ basic/server.js
@@ -74,3 +74,9 @@ app.delete('/delete', function (요청, 응답){
     });
 });
 
+app.get('/detail/:id', function (요청, 응답){
+    db.collection('post').findOne({ _id: parseInt(요청.params.id) }, function(에러, 결과){
+        console.log(결과);
+        응답.render('detail.ejs', { data: 결과 });
+    });
+});
