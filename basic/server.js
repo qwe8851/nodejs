00dda5ecbbb27b9ac8eb9c54866f88b41c15fd6d
qwe8851 <dgh07027@naver.com>
2023-02-01 20:46:10 +0900

    ajax 삭제요청3
    서버에서 현재 응답status를 보내고 ejs파일에서 성공/실패 시 코드짜기

--- basic/server.js
+++ basic/server.js
@@ -69,6 +69,8 @@ app.delete('/delete', function (요청, 응답){
     요청.body._id = parseInt(요청.body._id);
     db.collection('post').deleteOne(요청.body, function(에러, 결과){
         console.log('삭제완료');
+        // 응답.status(200).send({ message : '성공했습니다.'});
+        응답.status(400).send({ message : '실패했습니다.' });
     });
 });
 
