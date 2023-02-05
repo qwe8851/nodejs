cfac2640297ec0780ea6df2850cfb1996f952f81
qwe8851 <dgh07027@naver.com>
2023-02-05 21:30:39 +0900

    .env파일에서 민감한 환경변수 관리하기

--- basic/todoapp/server.js
+++ basic/todoapp/server.js
@@ -8,21 +8,34 @@ app.use(metodOverride('_method'));
 app.set('view engine', 'ejs');
 
 app.use('public', express.static('public'));
+const dotenv = require('dotenv').config();
+
+
+// var db;
+// MongoClient.connect('mongodb+srv://dgh07027:thdtmdgml@0826@cluster0.0vpladk.mongodb.net/?retryWrites=true&w=majority', function (에러, client) {
+//     // 연결되면 할 일
+//     if(에러) return console.log(에러);
+//     db = client.db('todoapp');
+
+//     db.collection('post').insertOne({_id: 100, 이름: 'John', 나이: 20}, function(에러, 결과){
+//         console.log('저장완료');
+//     });
+
+//     app.listen(8080, function () {
+//         console.log('listening on 8080');
+//     });
+// });
 
 var db;
-MongoClient.connect('mongodb+srv://dgh07027:thdtmdgml@0826@cluster0.0vpladk.mongodb.net/?retryWrites=true&w=majority', function(에러, client){
-    // 연결되면 할 일
-    if(에러) return console.log(에러);
+MongoClient.connect(process.env.DB_URL, function(err, client){
+    if(err) return console.log(err);
     db = client.db('todoapp');
-
-    db.collection('post').insertOne({_id: 100, 이름: 'John', 나이: 20}, function(에러, 결과){
-        console.log('저장완료');
-    });
-
-    app.listen(8080, function () {
+    app.listen(process.env.PORT, function(){
         console.log('listening on 8080');
     });
-});
+})
+
+
 
 app.get('/', function (요청, 응답) {
     응답.sendFile(__dirname + '/index.html');
