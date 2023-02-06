be88bf207f4bb1231d24e60bcb8b00ead80e034a
qwe8851 <dgh07027@naver.com>
2023-02-06 22:48:49 +0900

    검색기능 만들기 3 : Search index를 사용해 검색기능 구현하기

--- basic/todoapp/server.js
+++ basic/todoapp/server.js
@@ -113,11 +113,20 @@ app.put('/edit', function(요청, 응답) {
     });
 });
 
-app.get('/serch', (요청, 응답) => {
-    console.log(요청.query);
-    db.collection('post').find({ 제목: 요청.query.value }).toArray((에러, 결과)=>{
-        console.log(결과);
-        응답.render('serch.ejs', {serch: 결과});
+app.get('/search', (요청, 응답) => {
+    // db.collection('post').find({ $text: { $search: 요청.query.value } }).toArray((에러, 결과)=>{
+    var 검색조건 = [{
+        $search: {
+            index: 'titleSearch',
+            text: {
+                query: 요청.query.value,
+                path: '제목'  // 제목날짜 둘다 찾고 싶으면 ['제목', '날짜']
+            }
+        }
+    }];
+    db.collection('post').aggregate(검색조건).toArray((에러, 결과)=>{
+        console.log("결과 : " + 결과);
+        응답.render('search.ejs', { search: 결과 });
     });
 });
 
