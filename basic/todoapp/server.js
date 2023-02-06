8f82bdea88dff8162c38353267caf05fb5f1472c
qwe8851 <dgh07027@naver.com>
2023-02-06 23:22:16 +0900

    회원기능을 포함한 게시판 구현

--- basic/todoapp/server.js
+++ basic/todoapp/server.js
@@ -54,41 +54,12 @@ app.get('/write', function (요청, 응답) {
     응답.render('write.ejs');
 });
 
-app.post('/add', function (요청, 응답) {
-    응답.send('전송완료');
-    
-    db.collection('counter').findOne({ name: '게시물갯수'}, function(에러, 결과){
-        console.log(결과.totalPost);
-        let 총게시물객수 = 결과.totalPost;
-
-        // 데이터 입력
-        db.collection('post').insertOne({ _id: 총게시물객수 + 1, 제목: 요청.body.title, 날짜: 요청.body.date }, function (에러, 결과) {
-            console.log('저장완료');
-            db.collection('counter').updateOne({ name: '게시물갯수' }, { $inc: { totalPost: 1 }}, function(에러, 결과){
-                if(에러){ return console.log(에러)}
-            })
-        });
-    });
-});
-
 app.get('/list', function (요청, 응답) {
     // 데이터 출력
     db.collection('post').find().toArray(function(에러, 결과){
         // 모든 데이터 출력
         console.log(결과);  // 1. db에서 찾은 자료를
         응답.render('list.ejs', { posts: 결과 });   // 2. ejs파일에 넣기
-    });
-
-});
-
-app.delete('/delete', function (요청, 응답){
-    console.log(요청.body);
-    
-    요청.body._id = parseInt(요청.body._id);
-    db.collection('post').deleteOne(요청.body, function(에러, 결과){
-        console.log('삭제완료');
-        // 응답.status(200).send({ message : '성공했습니다.'});
-        응답.status(400).send({ message : '실패했습니다.' });
     });
 });
 
@@ -169,14 +140,6 @@ function 로그인했니(요청, 응답, next){
     }
 }
 
-
-
-
-
-
-
-
-
 // local Strategy 인증 방법
 passport.use(new LocalStrategy({
     usernameField: 'id',    // form에 id를 가진 것
@@ -207,3 +170,48 @@ passport.deserializeUser(function (아이디, done) { // 이 세션 데이터를
         done(null, 결과);   //{id:test, pw:test}
     });
 });
+
+app.post('/register', function(요청, 응답){
+    db.collection('login').findOne({id: 요청.body.id}, function(에러, 결과){
+        console.log(결과);
+        if (결과 != null){
+            응답.send('아이디 중복');
+        } else {
+            db.collection('login').insertOne({ id: 요청.body.id, pw: 요청.body.pw }, function (에러, 결과) {
+                응답.redirect('/');
+            });
+        }
+    });
+});
+
+app.post('/add', function (요청, 응답) {
+    응답.send('전송완료');
+
+    db.collection('counter').findOne({ name: '게시물갯수' }, function (에러, 결과) {
+        console.log(결과.totalPost);
+        let 총게시물객수 = 결과.totalPost;
+        let 저장목록 = { _id: 총게시물객수 + 1, 작성자: 요청.user._id, 제목: 요청.body.title, 날짜: 요청.body.date }
+        // 데이터 입력
+        db.collection('post').insertOne(저장목록, function (에러, 결과) {
+            console.log('저장완료');
+            db.collection('counter').updateOne({ name: '게시물갯수' }, { $inc: { totalPost: 1 } }, function (에러, 결과) {
+                if (에러) { return console.log(에러) }
+            })
+        });
+    });
+});
+
+app.delete('/delete', function (요청, 응답) {
+    console.log(요청.body);
+    요청.body._id = parseInt(요청.body._id);
+
+    let 삭제할데이터 = { _id: 요청.body._id, 작성자: 요청.user._id }
+
+    // 요청.body에 담겨온 게시물번호를 가진 글을 db에서 찾아 삭제
+    db.collection('post').deleteOne(요청.body, function (에러, 결과) {
+        console.log('삭제완료');
+        if (결과) { console.log("결과 : " + 결과) };
+        응답.status(200).send({ message: '성공했습니다.' });
+    });
+});
+
